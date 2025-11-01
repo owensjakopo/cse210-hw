daba9120e512c0968d8eca4d7eb4695d2ff6f0ae
owensjakopo <ojakopo1@byupathway.edu>
2025-11-01 18:54:34 +0200

    Finished Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your score (0-100): ");
+        string input = Console.ReadLine();
+        int score = int.Parse(input);
+
+        if (score < 0 || score > 100)
+        {
+            Console.WriteLine("Error: Score must be between 0 and 100.");
+            return;
+        }
+
+        string letter = "";
+        string symbol = "";
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //the + or minus symbol.
+        if (letter != "F")
+        {
+            int lastDigit = score % 10;
+            if (lastDigit >= 7 && letter != "A")
+            {
+                symbol = "+";
+            }
+            else if (lastDigit <= 3)
+            {
+                symbol = "-";
+            }
+        }
+
+
+        Console.WriteLine($"Your letter grade is: {letter}{symbol}");
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. Try again next time!");
+        }
     }
 }
