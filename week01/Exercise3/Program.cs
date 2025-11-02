d1590e9a7588f3b8a8ba673088b7184806a409cb
owensjakopo <ojakopo1@byupathway.edu>
2025-11-02 13:35:26 +0200

    Finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int yourNumber = -1;
+        int guessCount = 0;
+
+        while (yourNumber != magicNumber)
+        {
+            Console.Write("Guess the magic number between 1 and 100: ");
+            yourNumber = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (yourNumber == magicNumber)
+            {
+                Console.WriteLine($"You guessed it! It took you {guessCount} tries.");
+            }
+            else if (yourNumber > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("Higher!");
+            }
+        }
     }
 }
