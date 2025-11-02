c869a148d21b04cf39f1503f91596f46926c44d3
owensjakopo <ojakopo1@byupathway.edu>
2025-11-02 15:22:54 +0200

    Finished Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
-    }
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers and type 0 when done: ");
+        Console.WriteLine();
+        
+
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string response = Console.ReadLine();
+
+            number = int.Parse(response);
+
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        }
+        
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int largest = numbers.Max();
+
+        Console.WriteLine();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        
+       }  
 }
