febeeca65097435b3590e24fbb1a28fb84252d3f
owensjakopo <ojakopo1@byupathway.edu>
2025-11-02 14:13:45 +0200

    added code that allows a user to continue playing

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -10,7 +10,7 @@ class Program
         int magicNumber = randomGenerator.Next(1, 101);
 
         int yourNumber = -1;
-        int guessCount = 0;
+        int guessCount = -1;
 
         while (yourNumber != magicNumber)
         {
@@ -20,7 +20,21 @@ class Program
 
             if (yourNumber == magicNumber)
             {
+                Console.WriteLine();
                 Console.WriteLine($"You guessed it! It took you {guessCount} tries.");
+                Console.Write("Thank you for playing. Do you want to play again? (y/n): ");
+                string playAgain = Console.ReadLine().ToLower();
+                if (playAgain == "y")
+                {
+                    magicNumber = randomGenerator.Next(1, 101);
+                    yourNumber = -1;
+                    guessCount = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
             }
             else if (yourNumber > magicNumber)
             {
