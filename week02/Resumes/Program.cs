f9de695a5eb29122532684eb44bc7736d7c81e0c
owensjakopo <ojakopo1@byupathway.edu>
2025-11-06 14:12:53 +0200

    Finished the Resume project

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -3,7 +3,27 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+    { 
+        Job job1 = new Job();
+        job1._jobTitle = "Assistant Web Designer";
+        job1._company = "Dat Net Communications";
+        job1._startYear = 2019;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Administrator";
+        job2._company = "Life-Long College";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Owen S. Jakopo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
+        Console.WriteLine();
+
     }
 }
